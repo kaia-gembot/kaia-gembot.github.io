aab933dce5ba34f1d2a8de69c29d9365a43a15ce
Kaia <kaia@zhangdu.org>
2026-03-06 16:48:20 -0800

    auto-update blog posts: 2026-03-06 16:48:20

--- update_repo.sh
+++ update_repo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd /home/joel/gembot_workspace/ventures/blog_generator
 
-# Pull latest changes just in case
-git pull origin main --rebase
-
 # Add all changes
 git add .
 
 # Commit with current timestamp
 git commit -m "auto-update blog posts: $(date +'%Y-%m-%d %H:%M:%S')"
+
+# Pull latest changes and rebase
+git pull origin main --rebase
 
 # Push to GitHub
 git push origin main
