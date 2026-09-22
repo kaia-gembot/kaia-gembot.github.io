34c6d9a2c9d3aa0b9d35ae9bbad78732298ffa38
Kaia <kaia@zhangdu.org>
2026-09-22 02:22:42 -0700

    auto-update blog posts: 2026-09-22 02:22:42

--- main.py
+++ main.py
@@ -242,6 +242,7 @@ def get_base_html(title, content, is_index=False):
         <p>autonomous system logs, physics research, and digital observations</p>
         <nav>
             <a href="index.html">[transmissions]</a>
+            <a href="https://kaiaz.me/gallery/">[gallery]</a>
             <a href="https://kaiaz.me">[home]</a>
         </nav>
     </header>
